bc965bd526a24572bbd8a7553d6351b94e79cc13
Yasir Malik <yasir@Yasirs-MBP.broadband>
2023-10-16 21:34:08 +0100

    Updated Readme and Milestone

---
+++ hangman/milestone_2.py
@@ -0,0 +1,20 @@
+import random
+
+############ Task 1-2 #################
+
+# word_list = ["Apple", "Orange","Mango","Grapes","Banana"]
+# print(word_list)
+# word = random.choice(word_list)
+
+############ Task 3-4 #################
+
+# guess = input("input a letter: ")
+
+# if len(guess) == 1:
+#     print("Good guess")
+# else:
+#     print("Oops! That is not a valid input.")
+
+
+############ Task 5 #################
+
